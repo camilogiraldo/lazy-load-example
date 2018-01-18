1933a732e581fe611b7a2c442463dae6601b793a
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-01-17 21:35:49 -0500

    Created BigModule Module

---
+++ src/app/big-module/big-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BigModuleRoutingModule } from './big-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BigModuleRoutingModule
+  ],
+  declarations: []
+})
+export class BigModuleModule { }
