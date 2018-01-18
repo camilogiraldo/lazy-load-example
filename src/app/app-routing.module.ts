e48e586ef7f1ff92b22f1507dec76a7e8a706b97
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-01-17 21:16:20 -0500

    Added routing and our features in our components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { BigFeatureComponentComponent } from './big-feature-component/big-feature-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponentComponent},
+  { path: 'bigComponent', component: BigFeatureComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
